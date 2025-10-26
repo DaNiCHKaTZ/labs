2a0fe6a3f23a136fb8a9fbdb9881af6025894e84
DaNiCHKaTZ <168572078+DaNiCHKaTZ@users.noreply.github.com>
2025-10-26 20:08:41 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,50 +1,26 @@
 import numpy as np
 import os
-
+import sys
 from PIL import Image as img
 
 def get_line(start, end):
-    """Bresenham's Line Algorithm
-    Produces a list of tuples from start and end
-
-    >>> points1 = get_line((0, 0), (3, 4))
-    >>> points2 = get_line((3, 4), (0, 0))
-    >>> assert(set(points1) == set(points2))
-    >>> print points1
-    [(0, 0), (1, 1), (1, 2), (2, 3), (3, 4)]
-    >>> print points2
-    [(3, 4), (2, 3), (1, 2), (1, 1), (0, 0)]
-    """
-    # Setup initial conditions
     x1, y1 = start
     x2, y2 = end
     dx = x2 - x1
     dy = y2 - y1
-
-    # Determine how steep the line is
     is_steep = abs(dy) > abs(dx)
-
-    # Rotate line
     if is_steep:
         x1, y1 = y1, x1
         x2, y2 = y2, x2
-
-    # Swap start and end points if necessary and store swap state
     swapped = False
     if x1 > x2:
         x1, x2 = x2, x1
         y1, y2 = y2, y1
         swapped = True
-
-    # Recalculate differentials
     dx = x2 - x1
     dy = y2 - y1
-
-    # Calculate error
     error = int(dx / 2.0)
     ystep = 1 if y1 < y2 else -1
-
-    # Iterate over bounding box generating points between start and end
     y = y1
     points = []
     for x in range(x1, x2 + 1):
@@ -54,25 +30,24 @@ def get_line(start, end):
         if error < 0:
             y += ystep
             error += dx
-
-    # Reverse the list if the coordinates were swapped
     if swapped:
         points.reverse()
     return points
 
-stt = str(input("entrez cord start point"))
-ed = str(input("entrez coord end point"))
-start = [ int(i) for i in stt.split()]
-end = [ int(i) for i in ed.split()]
-L=get_line( (start[0],start[1]),(end[0],end[1]) )
+# Чтение аргументов
+if len(sys.argv) < 5:
+    print("Usage: python 1.py x1 y1 x2 y2")
+    sys.exit(1)
 
-matrix=np.ones((1000,1000),dtype=np.uint8)
-for tuple in L:
-    i=tuple[0]
-    j=tuple[1]
-    matrix[i][j]=0
-# print(matrix)
-im = img.fromarray(255*matrix, mode='L')
+start = [int(sys.argv[1]), int(sys.argv[2])]
+end = [int(sys.argv[3]), int(sys.argv[4])]
+L = get_line((start[0], start[1]), (end[0], end[1]))
+
+matrix = np.ones((1000, 1000), dtype=np.uint8)
+for i, j in L:
+    matrix[i][j] = 0
+
+im = img.fromarray(255 * matrix, mode='L')
 dir_path = os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(dir_path,'line.png')
+file_path = os.path.join(dir_path, 'line.png')
 im.save(fp=file_path)
